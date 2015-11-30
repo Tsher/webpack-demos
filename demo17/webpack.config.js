6e9faf10af04ab70f3bbacb41e6a5d06520749b8
alichs <262747531@qq.com>
2015-11-30 15:48:52 +0800

    yes

--- demo17/webpack.config.js
+++ demo17/webpack.config.js
@@ -1,13 +1,12 @@
 module.exports= {
-	entry: ['./index.cjsx', 'file?name=index.html!jade-html!./index.jade']
-	output: {
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{ test: /\.styl$/, loaders: 'style-loader!css-loader!stylus-loader'},
-			{ test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
-			{ test: /\.coffee$/, loader: 'coffee' },
-		]
-	}
+  entry: ['./index.cjsx', 'file?name=index.html!jade-html!./index.jade'],
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader'},
+      { test: /\.cjsx$/, loaders: ['coffee', 'cjsx']},
+    ]
+  }
 }
